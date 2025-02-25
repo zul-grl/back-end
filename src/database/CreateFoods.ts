23e5b5bce26aeab34ec8701b294bb3b05c18179a
zul-grl <zulgerel06n@gmail.com>
2025-02-25 10:37:37 +0800

    add

---
+++ src/database/CreateFoods.ts
@@ -0,0 +1,13 @@
+// import { MongoClient } from "mongodb";
+// import { configDotenv } from "dotenv";
+// export async function createFoods() {
+//     configDotenv();
+//   const uri = process.env.MONGODB_URL || "";
+//   const client = new MongoClient(uri);
+//   try {
+//     await client.connect();
+//     const dbName = "sample_mflix";
+//     const collectionName = "movies";
+//     const database = client.db(dbName);
+//     const collection = database.collection(collectionName);
+// }}
